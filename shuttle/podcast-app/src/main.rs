3cf8661fa14181ab6cda82c123c4134f2c2b5d56
Advik <adviksingh6632@gmail.com>
2024-02-12 04:54:36 +0530

    feat: hello rust

---
+++ shuttle/podcast-app/src/main.rs
@@ -0,0 +1,90 @@
+use axum::{routing::get, Router};
+use anyhow::Result;
+use xml::{EventReader, XmlEvent};
+use std::io::BufReader;
+
+enum ParseState {
+    Start,
+    InTitle,
+    InDescription,
+}
+
+struct Podcast {
+    title: String,
+    description: String,
+    audio_file: Option<String>,
+}
+
+impl Podcast {
+    fn new() -> Self {
+        Self {
+            title: String::new(),
+            description: String::new(),
+            audio_file: None,
+        }
+    }
+}
+
+fn read_podcast_from_xml(url: &str) -> Result<Vec<Podcast>> {
+    let mut results = Vec::new();
+
+    let data = reqwest::get(url).await?.text().await?;
+    let parser = EventReader::new(BufReader::new(data.as_bytes()));
+
+    let mut state = ParseState::Start;
+    let mut podcast = Podcast::new();
+
+    for event in parser {
+        match event {
+            Ok(XmlEvent::StartElement {
+                name, attributes, ..
+            }) => match name.local_name.as_str() {
+                "title" => state = ParseState::InTitle,
+                "description" => state = ParseState::InDescription,
+                "enclosure" => {
+                    podcast.audio_file = attributes.into_iter().find_map(|attr| {
+                        if attr.name.local_name == "url" {
+                            Some(attr.value)
+                        } else {
+                            None
+                        }
+                    });
+                }
+                _ => {}
+            },
+            Ok(XmlEvent::CData(content)) => match state {
+                ParseState::InTitle => {
+                    podcast.title = content;
+                    state = ParseState::Start;
+                }
+                ParseState::InDescription => {
+                    podcast.description = content;
+                    state = ParseState::Start;
+                }
+                _ => {}
+            },
+            Ok(XmlEvent::EndElement { name }) => {
+                if name.local_name == "item" {
+                    results.push(podcast);
+                    podcast = Podcast::new();
+                    state = ParseState::Start;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    Ok(results)
+}
+
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/", get(hello_world));
+
+    Ok(router.into())
+}
